7b8a302edbf2cfcfa0bfaf0092728b6753245cad
rashmi-kulkarni-ct402 <158051740+rashmi-kulkarni-ct402@users.noreply.github.com>
2024-02-08 12:24:20 +0530

    Update TxtWriter.cpp

--- Obj_To_Txt/src/TxtWriter.cpp
+++ Obj_To_Txt/src/TxtWriter.cpp
@@ -20,9 +20,9 @@ void Shapes3D::TxtWriter::write(std::string &filePath, Triangulation &triangulat
 
     for (const Triangle &triangle : triangles)
     {
-        outFile << points[triangle.index1() - 1].x() << " " << points[triangle.index1() - 1].y() << " " << points[triangle.index1() - 1].z() << std::endl;
-        outFile << points[triangle.index2() - 1].x() << " " << points[triangle.index2() - 1].y() << " " << points[triangle.index2() - 1].z() << std::endl;
-        outFile << points[triangle.index3() - 1].x() << " " << points[triangle.index3() - 1].y() << " " << points[triangle.index3() - 1].z() << std::endl;
+        outFile << points[triangle.index1()].x() << " " << points[triangle.index1()].y() << " " << points[triangle.index1()].z() << std::endl;
+        outFile << points[triangle.index2()].x() << " " << points[triangle.index2()].y() << " " << points[triangle.index2()].z() << std::endl;
+        outFile << points[triangle.index3()].x() << " " << points[triangle.index3()].y() << " " << points[triangle.index3()].z() << std::endl;
     }
     outFile.close();
     std::cout << "Data writting from .obj to .txt file successful!" << std::endl;
