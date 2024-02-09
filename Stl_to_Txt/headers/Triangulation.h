1db331f20a361c89e7760e1cbee04745955d7799
rashmi-kulkarni-ct402 <158051740+rashmi-kulkarni-ct402@users.noreply.github.com>
2024-02-09 15:44:05 +0530

    update to Triangulation.h
    Triangulation.h --> 
    1. added function declarations for
    void addUniquePointToTriangulation(const Point3D point);
    void addTriangleToTriangulation(const Triangle triangle);

--- Stl_to_Txt/headers/Triangulation.h
+++ Stl_to_Txt/headers/Triangulation.h
@@ -11,8 +11,10 @@ namespace Shapes3D
         Triangulation();
         ~Triangulation();
 
-        std::vector<Point3D> &uniquePoints();
-        std::vector<Triangle> &triangles();
+        std::vector<Point3D> uniquePoints() const;
+        std::vector<Triangle> triangles() const;
+        void addUniquePointToTriangulation(const Point3D point);
+        void addTriangleToTriangulation(const Triangle triangle);
 
     private:
         std::vector<Point3D> mUniquePoints;
